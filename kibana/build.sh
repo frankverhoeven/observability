7a578b41ab0e17a9717d6ca5b2d670c65b8dff3d
Frank Verhoeven <frankverhoeven@me.com>
2024-02-29 09:00:42 +0100

    fix: Build

--- kibana/build.sh
+++ kibana/build.sh
@@ -1,5 +1,5 @@
-KIBANA_VERSION=$(cat kibana_version)
+#wget --quiet -c https://artifacts.elastic.co/downloads/kibana/kibana-8.12.2-linux-x86_64.tar.gz -O - | tar -xz
 wget --quiet -c https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz -O - | tar -xz
-rm kibana-$KIBANA_VERSION-$KIBANA_PLATFORM/config/kibana.yml
-mv kibana-$KIBANA_VERSION-$KIBANA_PLATFORM kibana
+rm kibana-$KIBANA_VERSION/config/kibana.yml
+mv kibana-$KIBANA_VERSION kibana
 mv _config/kibana.yml kibana/config/kibana.yml
